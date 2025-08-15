b807180dc0008aba26b65020aff4edb41d62fe72
Diegopdev23 <pilidiegoadrian@gmail.com>
2025-08-15 15:00:02 -0300

    agrega funciones y validación de input

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
-// Declaracion de variables
+// Declaracion del array
 let amigos =[];
 console.log(amigos);
 
-//Funcion agregar amigos
+//Funcion agregar amigos y validacion
 function agregarAmigo() {
     let numeroAmigos = document.getElementById("amigo").value;
     console.log(numeroAmigos);
-     if (numeroAmigos.trim()==="") {
-        alert("Por favor,inserte un nombre.");
+      if (numeroAmigos.trim()===""|| /^\d+$/.test(numeroAmigos.trim())) {
+        alert("Por favor,ingrese un nombre valido.");
+      }
+      else if (/[!@#$%^&*(),.?":{}|<>\\[\]=~`_+\-]/.test(numeroAmigos)){
+        alert("Por favor,ingrese un nombre valido.");
+      }
+        else{ 
+        amigos.push(numeroAmigos);
+    }
         
-    } else{
-         amigos.push(numeroAmigos);
-
-     }
      limpiarCampoEntrada();
      ubicarAmigosEnLista();
 }
 
 function limpiarCampoEntrada() {
-    let restaurarCampo = document.getElementById("amigo").value = ""
+     restaurarCampo = document.getElementById("amigo").value = ""
     
 }
  //Funcion para actualizar lista de amigos
  function ubicarAmigosEnLista() {
-    let lista= document.getElementById("listaAmigos");
+    let lista = document.getElementById("listaAmigos");
     lista.innerHTML=""
     console.log(lista)
         for(let i=0; i<amigos.length; i++){ 
@@ -33,6 +36,57 @@ function limpiarCampoEntrada() {
 
            
            }
-        
-    
+           
  }
+ //Funcion para sortear amigos
+ function sortearAmigo() {
+    let sorteo = document.getElementById("resultado");
+    if (amigos.length===0) { 
+        alert("No hay amigos que sortear");
+        return;
+    } 
+    let aleatorioNumero = Math.floor(Math.random()*amigos.length)
+    let numeroSeleccionado = amigos[aleatorioNumero] 
+     
+    sorteo.innerHTML = `Tu amigo secreto es:"${numeroSeleccionado}"`
+    deshabilitarBotones()
+  } 
+   //Funciones para botones
+  function deshabilitarBotones() {
+    document.getElementById("sorteando").setAttribute("disabled","true");
+    document.getElementById("agregar").setAttribute("disabled","true");
+  }
+  function habilitarBotones() {
+    document.getElementById("sorteando").removeAttribute("disabled");
+    document.getElementById("agregar").removeAttribute("disabled");
+  }
+
+  //Funcion para reiniciar juego
+  function nuevoSorteo() {
+    habilitarBotones();
+    document.getElementById("resultado").innerHTML = "";
+    document.getElementById("listaAmigos").innerHTML = "";
+    amigos = [];
+    document.getElementById("amigo").value = "";
+    
+    
+
+  }
+    
+    
+  
+   
+    
+    
+      
+    
+    
+      
+    
+    
+  
+  
+
+    
+    
+  
